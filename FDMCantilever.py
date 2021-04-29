fed477c0c7e3c1422b98b0e94b90e3a99df34033
Jamie Lee <jamie.lee183@gmail.com>
2021-04-29 20:13:41 +0100

    mechanical added and verified, sensitivity analysis added

--- FDMCantilever.py
+++ FDMCantilever.py
@@ -3,31 +3,38 @@ import mpmath as mp
 
 
 class FDMCantilever:
-    def __init__(self, cantileverSettings, noElements=100,geomertyMods={}, coatings={},coatingMods={}):
+    def __init__(self, cantileverSettings, noElements={'Number of Elements':100},geomertyMods={}, coatings={},coatingMods={},mechanical={}):
 
+        self.cantileverSettings = cantileverSettings
         self.cantilever = {
             'w'  : cantileverSettings["Width"],
             'l'  : cantileverSettings["Length"],
             't'  : cantileverSettings["Thickness"],
             'E'  : cantileverSettings["Youngs Modulus"],
             'vs' : cantileverSettings["Possion's Ratio"],
+            'angle':[cantileverSettings["angle cantilever"], cantileverSettings["angle degrees const"],cantileverSettings["angle rad"]],
+            'angle change': [cantileverSettings["angle change"],cantileverSettings["changing angle function"]],
+            'Part Angle': cantileverSettings["Part Angle Cantilever"]
         }
-        
+        self.mechanical  = {"Force":0,"Offset Force":False}if not mechanical else mechanical
 
-        self.noElements = noElements
+
+        self.noElements = noElements['Number of Elements']
         self.noNodes = self.noElements+1
         self.elementLength = self.cantilever["l"]/self.noElements
 
         #setup node arrays
-        self.nodeXCoords = np.arange(0,self.noNodes,1)*self.elementLength #TODO: variable mesh
-        self.nodeCentroidCoords = self.nodeXCoords-(self.elementLength/2) #TODO: variable mesh
+        self.nodeXCoords = np.arange(0,self.noNodes,1)*self.elementLength #ne_x? #TODO: variable mesh
+        self.nodeCentroidCoords = self.nodeXCoords-(self.elementLength/2) #ne_xc #TODO: variable mesh
         self.nodeCentroidCoords[0] = 0
-        
-        self.nodeAlongCantileverCoords = self.nodeXCoords if not cantileverSettings["Part Angle Cantilever"]["PartAngle"] else np.zeros(self.noNodes)
 
-        self.baseElementXLenArr = np.ones(self.noNodes)*self.cantilever["l"]
+        self.cantilever['angle change'][1] = lambda: cantileverSettings["changing angle function"](x=self.nodeXCoords)
+        
+        self.nodeAlongCantileverCoords = self.nodeXCoords if not cantileverSettings["Part Angle Cantilever"]["Part Angle"] else np.zeros(self.noNodes)   #n_A
+
+        self.baseElementXLenArr = np.ones(self.noNodes)*self.elementLength    #Base Element X-Length Array #Le_xa
         self.baseElementXLenArr[0] = 0
-        self.baseCantileverXLen = self.cantilever['w']
+        self.baseCantileverLen = self.cantilever['l']
 
         #pristmatic widths
         self.widthArr = np.ones(self.noNodes)*self.cantilever['w']
@@ -51,6 +58,20 @@ class FDMCantilever:
                 if coatingMods:
                     self.modify(modDict=coatingMods, arr=self.coatingWidthArr)
 
+            #TODO: Composite Mechanical <-- coating changes + underCoating goes in here
+            self.compositeMechanicalCalc()
+
+
+        self.partAngledAngleArr = np.zeros(self.noNodes)    #PAr
+        self.angleCantilever()
+
+        # self.mechanicalInputCalc()
+        # self.mechanicalOutCalc()
+
+    def doMechanical(self):
+        self.mechanicalInputCalc()
+        # self.mechanicalOutCalc()
+        return self.mechanicalOutCalc()
         
 
     def modify(self, modDict, arr):
@@ -61,14 +82,27 @@ class FDMCantilever:
         if 'Gaps/Width' in modDict.keys() :
             if modDict['Gaps/Width']["Gap/Width Change"]:
                 params = modDict['Gaps/Width']
-                params.pop('Gap/Width Change')
+                params = {x: modDict['Gaps/Width'][x] for x in modDict['Gaps/Width'] if x not in {'Gap/Width Change'}} #eliminate True from dict 
 
                 for key in params.keys():
                     width = params[key]
                     if not width['active']: 
                         continue 
                     self.addWidthChange(arr=arr,params=width)
-        
+
+        # if 'coating change' in modDict.keys() :   #TODO: IMPLEMENT coating changes
+        #     if modDict['coating change']["coating change"]:
+        #         params = modDict['Gaps/Width']
+        #         params.pop('Gap/Width Change')
+
+        #         for key in params.keys():
+        #             width = params[key]
+        #             if not width['active']: 
+        #                 continue 
+        #             self.addWidthChange(arr=arr,params=width)
+
+            
+
 
     def addTaper(self, arr, params):
         TM=(self.cantilever['w']-params['end width'])/(params['start']-params['end'])
@@ -91,8 +125,152 @@ class FDMCantilever:
             if self.nodeCentroidCoords[i]>params['startX'] and self.nodeCentroidCoords[i]<=params['endX']:
                 arr[i]=arr[i]-params['start change']-(GM*(self.nodeCentroidCoords[i]-params['startX']))
 
+    def angleCantilever(self):
+        self.angledArr = self.cantilever['angle change'][1]() if self.cantilever['angle change'][0] else np.zeros(self.noNodes)
+        
+        if self.cantilever['Part Angle']['Part Angle']:
+            PE = self.cantilever['Part Angle']['end']
+            PS = self.cantilever['Part Angle']['start']
+            PAd = self.cantilever['Part Angle']['degrees'] 
+
+            self.baseCantileverLen = (self.cantilever['l'] - (PE-PS)) + ((PE-PS)/np.cos(np.deg2rad(PAd))) #Total Cantilever Along-Length (m)
+            self.partAngledEndAlongLength = PS+((PE-PS)/np.cos(np.deg2rad(PAd)))  #Part Angled End Along-Length (m)
+            #Part Angled Substrate Element X-Length (m)
+            PS_N = next(i for i in range(1,self.noNodes,1) if self.nodeCentroidCoords[i-1]<=PS and self.nodeCentroidCoords[i]>PS)
+            PE_N = next(i if self.nodeCentroidCoords[i-1]<=PE and self.nodeCentroidCoords[i]>PE else self.noNodes for i in range(PS_N,self.noNodes,1))
+            Pe_x=(self.partAngledEndAlongLength-self.nodeXCoords[PS_N-1])/(PE_N-PS_N)
+            #Part Angled Element X-Length Array
+            self.baseElementXLenArr = np.asarray([Pe_x if self.nodeCentroidCoords[i]>PS and self.nodeCentroidCoords[i]<=PE else self.baseElementXLenArr[i] for i in range(0,self.noNodes,1)])
+
+            self.partAngledAngleArr = np.asarray([np.deg2rad(PAd) if self.nodeCentroidCoords[i]>=PS and self.nodeCentroidCoords[i]<=PE else 0 for i in range(0,self.noNodes,1)]) 
+
+            for i in range(1,self.noNodes,1):
+                self.nodeAlongCantileverCoords[i] =  self.nodeAlongCantileverCoords[i-1]+self.baseElementXLenArr[i]
+
+    def mechanicalInputCalc(self):
+        if self.mechanical["Offset Force"]:
+            PS = self.cantilever['Part Angle']['start']
+            PE = self.cantilever['Part Angle']['end']
+            OF_Px=self.mechanical['Force application X point']
+            
+            OF_PA = ((PS+((OF_Px-PS)/np.cos(self.partAngledAngleArr))) if self.cantilever['Part Angle']['Part Angle'] and OF_Px>PS and OF_Px<=PE else OF_Px)
+            self.offsetForceZeroArr=[0 for i in range(self.noNodes) if self.nodeXCoords[i]>OF_Px] 
+
+        F_Px=(OF_Px if self.mechanical["Offset Force"] else self.cantilever['l'])
+        #Force Application Along-Point
+        F_PA=(OF_PA if self.mechanical["Offset Force"] else self.baseCantileverLen)
+        #Force Application Point Array
+        self.forceAppPointArr=np.asarray([F_PA if self.cantilever['Part Angle']['Part Angle'] and self.nodeCentroidCoords[i]>self.cantilever['Part Angle']['start'] and self.nodeCentroidCoords[i]<=self.cantilever['Part Angle']['end'] else F_Px for i in range(0,self.noNodes)])
+        
+
+    def mechanicalOutCalc(self):
+
+        angleConst = np.cos(self.partAngledAngleArr+self.cantilever['angle'][2]+self.angledArr)**2
+        #Spring Constants
+        #Spring Constant component of Y displacement
+        springUY = (self.baseElementXLenArr**2/(self.cantilever['E']*self.element2ndMomentOfArea)*((self.baseElementXLenArr/3)  \
+                            +((self.forceAppPointArr-self.nodeAlongCantileverCoords)/2)))                                       \
+                            *angleConst
+       
+        #Spring Constant Component for Rotational Y Displacement
+        springRXY = (self.baseElementXLenArr**2/(self.cantilever['E']*self.element2ndMomentOfArea)*((self.baseElementXLenArr/2)  \
+                            +self.forceAppPointArr-self.nodeAlongCantileverCoords))                                       \
+                            *angleConst
+       
+        if self.mechanical['Offset Force']:
+            springUY[self.noNodes-len(self.offsetForceZeroArr):self.noNodes]=self.offsetForceZeroArr
+            springRXY[self.noNodes-len(self.offsetForceZeroArr):self.noNodes]=self.offsetForceZeroArr
+
+        #Total Spring Constant
+        springRUY=np.zeros(self.noNodes)
+        for i in range(1,self.noNodes):
+            # if VM == "Y" and Le_xa[i] != SM_Le_x: #TODO: variable mesh
+            #     k_R_UY[i]=(np.sum(k_RXY[0:VM_N])/SM_Le_x*RM_Le_x)+np.sum(k_RXY[VM_N:i])
+            # else:
+            springRUY[i]=np.sum(springRXY[0:i]) 
+
+        springT=springUY+springRUY
+        springT[0]= np.Infinity
+        springE = 1/springT
+        springE[0] = np.Infinity
+
+        springAlong=np.zeros(self.noNodes)
+        for i in range(1,self.noNodes+1,1):
+            ke = 1/springE
+            ke[i:self.noNodes]=0
+            springAlong[0]=0
+            if np.sum(ke) == 0:                                                 
+                springAlong[i-1]=0                                                     #Remove division by zero error
+            else:
+                springAlong[i-1]=1/np.sum(ke)  
+        
+        
+        #Displacements
+        F = self.mechanical['Force']
+        #Y displacement
+        dispY = ((F*self.baseElementXLenArr**3/(3*self.cantilever['E']*self.element2ndMomentOfArea))            \
+                    +(F*(self.forceAppPointArr-self.nodeAlongCantileverCoords)*self.baseElementXLenArr**2       \
+                    /(2*self.cantilever['E']*self.element2ndMomentOfArea)))                                      \
+                    *angleConst**2
+        #Rotational displacement
+        dispR=((F*self.baseElementXLenArr**2/(2*self.cantilever['E']*self.element2ndMomentOfArea))                  \
+                    +(F*(self.forceAppPointArr-self.nodeAlongCantileverCoords)*self.baseElementXLenArr           \
+                    /(self.cantilever['E']*self.element2ndMomentOfArea)))                                       \
+                    *angleConst**2
+
+        if self.mechanical['Offset Force']:
+            dispY[self.noNodes-len(self.offsetForceZeroArr):self.noNodes]=self.offsetForceZeroArr
+            dispR[self.noNodes-len(self.offsetForceZeroArr):self.noNodes]=self.offsetForceZeroArr
+
+        
+        #rotational Y displacement
+        dispRUY = np.zeros(self.noNodes)
+        dispUY= np.zeros(self.noNodes)
+        dispUY= np.tan(dispR)*self.baseElementXLenArr
+
+
+        # dispUY = np.array(dispUY,dtype=float)
+        for i in range(1,self.noNodes):
+            # if VM == "Y" and Le_xa[i] != SM_Le_x: #TODO: variable mesh
+            #     R_UY[i]=(np.sum(UY[0:VM_N])/SM_Le_x*RM_Le_x)+np.sum(UY[VM_N:i])
+            # else:
+            dispRUY[i]=np.sum(dispUY[0:i])   
+
+        #Displacement Y total
+        dispYTotal = dispY+dispRUY
+        #Y displacement along cantilever
+        dispUYAlong = np.zeros(self.noNodes)
+        #Along cantilever Rotational Displacement
+        dispRXYAlong = np.zeros(self.noNodes)
+        for i in range(1,self.noNodes):
+            dispUYAlong[i]=np.sum(dispYTotal[0:i+1])
+            dispRXYAlong[i]=np.sum(dispR[0:i+1])
 
     
+
+        self.mechOut = {
+            'spring along' : springAlong,
+            'spring total': 1/np.sum(1/springE),
+            'disp along Y': dispUYAlong,
+            'disp along R': dispRXYAlong,
+            'disp end Y' : np.sum(dispYTotal),
+        }
+        print('spring: ',self.mechOut['spring total'])
+        print('end disp: ',self.mechOut['disp end Y'])
+        # print('disp: ',self.mechOut['disp along Y'])
+        return self.mechOut
+
+
+        
+
+    def compositeMechanicalCalc(self):
+        pass
+
+
+
+
+
+
 
     def derivitave1(self,eq,boundries,interval):
         t=np.linspace(interval[0],interval[-1], self.noElements+1)
@@ -132,7 +310,21 @@ class FDMCantilever:
         print(b)
         return t, np.linalg.solve(A,b)
 
+class FDMCantileverSensitivity:
+    def __init__(self, variableDict, variable, arr=None, lims=None, steps=100, **kwarg):
+        print(f"Sweeping variable {variable} in {variableDict}, from array {arr} ")
 
+        if arr is not None:
+            out = []
+            for i,val in enumerate(arr):
+                kwarg[variableDict][variable] = val
+                obj = FDMCantilever(**kwarg)
+                out.append([val,obj.doMechanical()['disp end Y']])
+
+
+
+
+        # do = FDMCantilever(**kwarg)
 
 if __name__ == '__main__':
     import numpy as np
--- FDMCantilever_settings.py
+++ FDMCantilever_settings.py
@@ -1,12 +1,12 @@
 
-from FDMCantilever import FDMCantilever
+from FDMCantilever import FDMCantilever, FDMCantileverSensitivity
 
 if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
 
     elements = {
-        "Number of Elements" :121,
+        "Number of Elements" :500,
     }
 
     """
@@ -19,22 +19,36 @@ if __name__ == '__main__':
         "Possion's Ratio" : 0.263,
         "Youngs Modulus" : 2.4162e11,
     })
+
     
     cantileverSettings.update({
-        "Part Angle Cantilever":{  "PartAngle": False,
+        'angle cantilever': False,
+        'angle degrees const' : 13,
+        'angle change': False,
+        'changing angle function' : lambda x: (1.4*x**0.4), #func as lambda
+
+        "Part Angle Cantilever":{  "Part Angle": False,
                                     "start"  : 139e-6, 
-                                    "End"    : cantileverSettings["Length"], 
+                                    "end"    : cantileverSettings["Length"], 
                                     "degrees": 56, 
-                                    "radians": np.deg2rad(56)
                                 }
     })
+    
+    """
+    ---------------------------Mechanical----------------
+    """
+    mechanical = {
+        "Force": 1e-9,
+        "Offset Force": False,
+        "Force application X point": 106E-6
+    }
     """
     ----------------------------Geometric Modifications--------------------------------
     """
     geomertyMods = {}  #can be of type 'gap' or 'taper'
 
     geoGapWidth ={
-        "Gap/Width Change" : False
+        "Gap/Width Change" : True
     }
     if geoGapWidth["Gap/Width Change"]:    #add as many gaps as you like
         geoGapWidth.update({
@@ -43,12 +57,12 @@ if __name__ == '__main__':
                     "active"        :True,
                     "startX"        :8e-6,
                     "endX"          :20e-6,#cantileverSettings["Cantilever Length"],
-                    "start change"  :10e-6,
-                    "end change"    :10e-6,
+                    "start change"  :100e-6,
+                    "end change"    :1e-6,
                     },
 
             "Change2" : {
-                    "active"        :True,
+                    "active"        :False,
                     "startX"        :110e-6,
                     "endX"          :cantileverSettings["Length"],
                     "start change"  :119e-6,
@@ -60,18 +74,12 @@ if __name__ == '__main__':
 
 
     geoTaper = {
-        "Taper" : False,
-        "parameters"    : { "start" : 110e-6,
+        "Taper" : True,
+        "parameters"    : { "start" : 90E-6,
                             "end"   : cantileverSettings["Length"],
                             "end width": 0e-6,
                         }
     }
-
-    if geoTaper["Taper"]:
-        geomertyMods["Taper"] = geoTaper
-
-    if geoGapWidth["Gap/Width Change"]:
-        geomertyMods['Gaps/Width'] = geoGapWidth
 
 
     """
@@ -79,7 +87,7 @@ if __name__ == '__main__':
     """
     
     coatings = {
-        "Coating": True,
+        "Coating": False,
         "startX" : 0,
         "endX"   : cantileverSettings['Length'],
         "Thickness" : 70-9,
@@ -118,23 +126,35 @@ if __name__ == '__main__':
                         }
     }
 
+
     if coatTaper["Taper"]:
         coatingMods["Taper"] = coatTaper
 
     if coatGapsWidths["Gap/Width Change"]:
         coatingMods['Gaps/Width'] = coatGapsWidths
 
- 
-    noElements = elements['Number of Elements']
+    if geoTaper["Taper"]:
+        geomertyMods["Taper"] = geoTaper
+
+    if geoGapWidth["Gap/Width Change"]:
+        geomertyMods['Gaps/Width'] = geoGapWidth
+
+    cantileverSettings['angle rad'] = np.deg2rad(cantileverSettings['angle degrees const']) if cantileverSettings['angle cantilever'] else 0
+    cantileverSettings['Part Angle Cantilever']['rad'] = np.deg2rad(cantileverSettings['Part Angle Cantilever']['degrees']) if cantileverSettings['Part Angle Cantilever']['Part Angle'] else 0
+
+    # noElements = elements['Number of Elements']
 
 
-
-    do = FDMCantilever(cantileverSettings=cantileverSettings, noElements=noElements, geomertyMods=geomertyMods, coatings=coatings,coatingMods=coatingMods)
-    eq = '-9'
-    interval = [0,5]
-    boundries = [None] * (interval[-1]+1)
-    boundries[0]=0
-    boundries[5]=50
+    variableDict = "mechanical"
+    variable = "Force"
+    arr = [1e-9,2e-9]
+    # do = FDMCantilever(cantileverSettings=cantileverSettings, noElements=elements, geomertyMods=geomertyMods, coatings=coatings,coatingMods=coatingMods, mechanical=mechanical)
+    do = FDMCantileverSensitivity(variableDict=variableDict,variable=variable,arr=arr,cantileverSettings=cantileverSettings, noElements=elements, geomertyMods=geomertyMods, coatings=coatings,coatingMods=coatingMods, mechanical=mechanical)
+    # eq = '-9'
+    # interval = [0,5]
+    # boundries = [None] * (interval[-1]+1)
+    # boundries[0]=0
+    # boundries[5]=50
 
     # print(boundries)
     # t,y = do.derivitave2(eq=eq, boundries=boundries, interval=interval)
